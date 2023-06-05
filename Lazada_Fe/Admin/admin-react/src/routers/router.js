3b3cfcd2fb420a79ffbf7aca7790acf2d884475a
DoanMinhCuong <minhcuong3859@gmail.com>
2023-06-05 18:41:20 +0700

    Lazada-Layout-admin

--- Lazada_Fe/Admin/admin-react/src/routers/router.js
+++ Lazada_Fe/Admin/admin-react/src/routers/router.js
@@ -1,16 +1,48 @@
 import {createBrowserRouter} from "react-router-dom";
 import Layout from "../pages/layout";
-
+import Index from "../pages";
+import UserIndex from "../pages/users";
+import UserCreate from "../pages/users/create";
+import UserEdit from "../pages/users/edit";
+import AuthLayout from "../pages/auth/authLayout";
+import Login from "../pages/auth/login";
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Layout />
-        // children:[
-        //     {
-        //         index: true,
-        //         element: <Index />,
-        //     },
-        // ]
+        element: <Layout />,
+        children:[
+            {
+                index: true,
+                element: <Index/>
+            },
+            {
+                path: "users",
+                children: [
+                    {
+                        index: true,
+                        element: <UserIndex />,
+                    },
+                    {
+                        path: "create",
+                        element: <UserCreate />,
+                    },
+                    {
+                        path: ":userId/edit",
+                        element: <UserEdit />,
+                    },
+                ]
+            }
+        ]
+    },
+    {
+        path: "/",
+        element: <AuthLayout />,
+        children: [
+            {
+                path: "/login",
+                element: <Login />,
+            },
+        ]
     }
    
 ]);
